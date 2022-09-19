ec4840eb5ceee0e62f6a0ba128b30e4f60f43168
Misha Tuesday <mishatuesday@gmail.com>
2022-09-19 17:40:34 -0400

    added javascript

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(clientStreet) {
+    return Math.abs(clientStreet-42) 
+} 
+
+function distanceFromHqInFeet(clientStreet) {
+    return Math.abs(clientStreet - 42) * 264
+}
+
+function distanceTravelledInFeet(start,end) {
+    return Math.abs(start-end)*264
+}
+
+function calculatesFarePrice (start,end) {
+    const distanceInFeet = Math.abs(start - end) * 264
+    if (distanceInFeet <= 400) return 0
+    else if (distanceInFeet > 2500) return "cannot travel that far"
+    else if (distanceInFeet < 2000) return (distanceInFeet - 400) * .02
+    else return 25
+
+}
