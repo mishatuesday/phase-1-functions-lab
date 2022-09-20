b1dfe4ff66eb0ead2df4a082e45f8711fe687665
Misha Tuesday <mishatuesday@gmail.com>
2022-09-20 11:45:39 -0400

    commit tree 2eb5f4c2db0413611cba3879991033684a016500

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(clientStreet) {
-    return Math.abs(clientStreet-42) 
-} 
+const distanceFromHqInBlocks = (clientStreet) => Math.abs(clientStreet-42) 
 
-function distanceFromHqInFeet(clientStreet) {
-    return Math.abs(clientStreet - 42) * 264
-}
+const distanceFromHqInFeet = (clientStreet) => Math.abs(clientStreet - 42) * 264
+
 
 function distanceTravelledInFeet(start,end) {
     return Math.abs(start-end)*264
 }
 
-function calculatesFarePrice (start,end) {
-    const distanceInFeet = Math.abs(start - end) * 264
-    if (distanceInFeet <= 400) return 0
-    else if (distanceInFeet > 2500) return "cannot travel that far"
-    else if (distanceInFeet < 2000) return (distanceInFeet - 400) * .02
-    else return 25
+// function calculatesFarePrice (start,end) {
+//     const distanceInFeet = Math.abs(start - end) * 264
+//     if (distanceInFeet <= 400) return 0
+//     else if (distanceInFeet > 2500) return "cannot travel that far"
+//     else if (distanceInFeet < 2000) return (distanceInFeet - 400) * .02
+//     else return 25
+// }
 
+function calculatesFarePrice (start,end) {
+    const distanceInFeet = Math.abs(start-end) * 264
+    switch (true) {
+        case (distanceInFeet <= 400):
+            return 0;
+            break;
+        case (distanceInFeet > 2500):
+            return "cannot travel that far";
+            break;
+        case (distanceInFeet <= 2000):
+            return (distanceInFeet - 400) * .02;
+            break;
+        case (distanceInFeet > 2000):
+            return 25;
+    }
 }
